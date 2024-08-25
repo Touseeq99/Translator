118ee499ab968fd2c4436a3811a6cf7dca38745f
Touseeq-PC <touseeqahmed2343@gmail.com>
2024-08-26 02:07:53 +0500

    model update

--- app.py
+++ app.py
@@ -80,6 +80,6 @@ text = st.text_input("Enter Your Text")
 
 if text:
     task = f"translation_{swapped_languages[from_lang]}_to_{swapped_languages[to_lang]}"
-    translator = pipeline(task=task, model="facebook/m2m100_418M")
+    translator = pipeline(task=task, model="alirezamsh/small100")
     output = translator(text)
     st.write("Translated:", output[0]['translation_text'])
